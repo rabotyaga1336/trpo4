e2be97a48302bb447db0628d27e0e0221f645e64
rabotyaga1336 <vrot.enot2017@gmail.com>
2022-05-30 20:28:33 +0300

    first commit

---
+++ trpo-maven-profile-log/src/main/java/isap/vstu/by/App.java
@@ -0,0 +1,13 @@
+package isap.vstu.by;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
